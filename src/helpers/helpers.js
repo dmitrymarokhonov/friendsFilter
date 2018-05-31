8cb44d49fa8f391045019935bdcc5f244d61608c
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2018-05-31 16:10:57 +0300

    Полностью переделал

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -3,13 +3,19 @@ const isMatching = (full, chunk) => {
 }
 
 const compareNames = (a, b) => {
+    if (a.last_name > b.last_name) {
+        return 1;
+    }
+    if (a.last_name < b.last_name) {
+        return -1;
+    } 
     if (a.first_name > b.first_name) {
         return 1;
     }
-    if (a.first_name < b.first_name) {
+    if (a.last_name < b.first_name) {
         return -1;
-    }
-
+    } 
+    
     return 0;
 }
 
